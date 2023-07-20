cae89970d99da3d3c670d7d7d6f9fadc693f04be
heejun <hj.yoo@odinue.net>
2023-07-20 22:06:03 +0900

    0720

---
+++ src/Study/Week_01/소수만들기.java
@@ -0,0 +1,46 @@
+package Study.Week_01;
+
+public class 소수만들기 {
+    static int solution(int[] nums) {
+        int answer = 0;
+        int num = 0;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int k = i + 1; k < nums.length - 1; k++) {
+                for (int m = k + 1; m < nums.length; m++) {
+                    num = nums[i] + nums[k] + nums[m];
+                    if (checkPrime(num)) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+
+    static boolean checkPrime(int num) {
+
+        if (num % 2 == 0) {
+            return false;
+        }
+
+        for (int i = 3; i < (Math.sqrt(num)) + 1; i = i + 2) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        int[] val1 = {1, 2, 3, 4};
+        int solution1 = solution(val1);
+        System.out.println("solution = " + solution1);
+
+        int[] val2 = {1, 2, 7, 6, 4};
+        int solution2 = solution(val2);
+        System.out.println("solution2 = " + solution2);
+
+    }
+}
